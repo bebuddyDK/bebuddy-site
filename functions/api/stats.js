59b34005aa11400fc8adb64e506d2e42fe22e4c8
BeBuddy <148059123+bebuddyDK@users.noreply.github.com>
2025-09-30 18:09:33 +0200

    Delete functions/api/stats.js

--- functions/api/stats.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet({ env }) {
-  const { results } = await env.DB
-    .prepare("SELECT file, count, last_at FROM downloads ORDER BY count DESC")
-    .all();
-  return new Response(JSON.stringify({ downloads: results || [] }), {
-    headers: { "content-type": "application/json; charset=utf-8" },
-  });
-}
