d0ed0b55595bfa452cb52adbc0607c3a7e406b3e
EstherKJ <148059123+EstherKJ@users.noreply.github.com>
2025-09-30 13:45:08 +0200

    Add files via upload

--- functions/get/bebuddy.js
+++ functions/get/bebuddy.js
@@ -1,11 +1,9 @@
 // /functions/get/bebuddy.js
-// Simpelt og robust: tæller i D1 og laver en 302-redirect.
-// Åbn i ny fane styres fra HTML med target="_blank".
-
 export async function onRequestGet({ env }) {
+  // Navn på "fil"/target i din D1 – skift gerne navnet, hvis du vil
   const target = 'google_drive_folder';
-  const driveUrl = 'https://drive.google.com/drive/folders/19YViyy-D3kVVO572zeP0-pJYm-1EKYx-?usp=sharing';
 
+  // Tilpas evt. tabel/kolonnenavne her, hvis din gamle tabel ikke hedder 'downloads'
   const sql = `
     INSERT INTO downloads (file, count, last_at)
     VALUES (?1, 1, datetime('now'))
@@ -17,9 +15,11 @@ export async function onRequestGet({ env }) {
   try {
     await env.DB.prepare(sql).bind(target).run();
   } catch (e) {
-    // Logger men redirecter uanset hvad
+    // Vi logger, men lader stadig brugeren komme videre til Drive
     console.error('DB error in /get/bebuddy:', e);
   }
 
+  // Redirect til din offentlige Google Drive-mappe
+  const driveUrl = 'https://drive.google.com/drive/folders/19YViyy-D3kVVO572zeP0-pJYm-1EKYx-?usp=sharing';
   return Response.redirect(driveUrl, 302);
 }
